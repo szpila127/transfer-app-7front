3f2ae83240c9dea08f1eac72c0889ba81958467b
szpila127 <inglot.seb@gmail.com>
2020-02-18 22:27:34 +0100

    First

---
+++ src/test/java/com/transfer/app7b/TransferApp7bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfer.app7b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferApp7bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
