b91b06af0c3fb13979bb8e9c494f3682fe8f9c74
szpila127 <inglot.seb@gmail.com>
2020-02-23 22:04:00 +0100

    AdminFull

--- src/main/java/com/transfer/app7b/view/HomeView.java
+++ src/main/java/com/transfer/app7b/view/HomeView.java
@@ -1,6 +1,9 @@
 package com.transfer.app7b.view;
 
 import com.transfer.app7b.service.CurrencyService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
@@ -9,12 +12,17 @@ import com.vaadin.flow.router.Route;
 @Route(value = "")
 public class HomeView extends VerticalLayout {
 
+    private Button adminButton = new Button("ADMIN",  VaadinIcon.KEY.create());
     private NumberField eurField = new NumberField();
     private NumberField usdField = new NumberField();
     private NumberField gbpField = new NumberField();
     private CurrencyService currencyService = new CurrencyService();
 
     public HomeView() {
+        adminButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        adminButton.addClickListener(event -> {
+            adminButton.getUI().ifPresent(ui -> ui.navigate("admin/users"));
+        });
         eurField.setLabel("EUR");
         eurField.setReadOnly(true);
         usdField.setLabel("USD");
@@ -22,7 +30,7 @@ public class HomeView extends VerticalLayout {
         gbpField.setLabel("GBP");
         gbpField.setReadOnly(true);
         HorizontalLayout currencies = new HorizontalLayout(eurField, usdField, gbpField);
-        add(currencies);
+        add(adminButton, currencies);
         setSizeFull();
         refreshCurrency();
     }
